c714278893a352c7d9af2556d934dc9d4d07fd6b
Kiril <alexbour82@gmail.com>
2022-06-11 10:59:38 -0300

    contact danger alert refactoing

--- frontend/src/components/sub-comp/index/contact.jsx
+++ frontend/src/components/sub-comp/index/contact.jsx
@@ -1,34 +1,50 @@
-import { motion } from 'framer-motion';
 import { createRef, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import AlertDanger from '../../AlertDanger';
 import url from './../../utils/url';
 
-const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: -50 },
-};
-
 export default function Contact(){
-    const [sentisOpen, changesentisOpen] = useState(true);
+    const [dangerAlert, setDangerAlert] = useState(false);
+    const [dangerText, setDangerText] = useState('')
+
     const emailRef = createRef()
     const subjectRef = createRef()
     const contentRef = createRef()
-    const alertRef = createRef()
 
     const sendemail = async () => {
+        const email = emailRef.current.value
+        const subject = subjectRef.current.value
+        const content = contentRef.current.value
+        
+        if (findDataErrors([email,subject,content])) {
+            alertError('Please fill all fields.')
+            return;
+        }
+        
         const response = await (await fetch(`${url()}/api/sendmail`, {
             method: 'POST',
             body: JSON.stringify({
-                EmailBack: emailRef.current.value,
-                Subject: subjectRef.current.value,
-                Content: contentRef.current.value
+                EmailBack: email,
+                Subject: subject,
+                Content: content
             })
         }
         )).json();
 
         checkresponse(response)
+    }
+
+    const findDataErrors = (li) => {
+        console.log(li);
+        li.forEach(data => {
+            if (data === '') {
+                console.log(data);
+                return true
+            }
+        });
+        return false
     }
 
     const checkresponse = (response) => {
@@ -40,53 +56,42 @@ export default function Contact(){
             return;
         }
         if (response.message) {
-            alert('fail', response.message)
+            alertError(response.message)
             return;
         }
-        alert('fail', "I'm sorry, something went wrong. Please wait, then try again. Or, reach out directly via my email.")
+        alertError( "I'm sorry, something went wrong. Please wait, then try again. Or, reach out directly via my email.")
     }
 
-    const alert = (type, msg) => {
-        const element = document.getElementById('SentAlert')
-
-        if (type === 'success') {
-            alert.classList.add('alert-success');
-            alert.innerHTML = msg;
-        } else{
-            alert.classList.add('alert-danger');
-            alert.innerHTML = msg;
-        }
-
-        changesentisOpen(true);
-            setTimeout(() => { 
-                changesentisOpen(false);
-                setTimeout(() => {
-                    alert.classList = 'mt-2 alert'
-                }, 1200);
-            }, 2000);
+    const alertError = (msg) => {
+        setDangerText(msg)
+        setDangerAlert(true);
+        setTimeout(() => { 
+            setDangerAlert(false);
+        }, 2000);
     }
 
     return (
         <div className='col-7 mt-3 contact'>
             <Form>
-            <Form.Group className="mb-3" ref={emailRef}>
+            <Form.Group className="mb-3">
                 <Form.Label>Your email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" ref={emailRef}/>
                 <Form.Text className="text-muted">
                 This will only be used to respond to your email.
                 </Form.Text>
             </Form.Group>
 
-            <Form.Group className="mb-3" ref={subjectRef}>
+            <Form.Group className="mb-3" >
                 <Form.Label>Subject</Form.Label>
-                <Form.Control type="text" placeholder="Subject" />
+                <Form.Control type="text" placeholder="Subject" ref={subjectRef}/>
             </Form.Group>
             
-            <FloatingLabel ref={contentRef} label="Comments">
+            <FloatingLabel label="Comments">
                 <Form.Control
                 as="textarea"
                 placeholder="Leave a comment here"
                 style={{ height: '250px' }}
+                ref={contentRef}
                 />
             </FloatingLabel>
             
@@ -94,14 +99,13 @@ export default function Contact(){
                 Submit
             </Button>
             </Form>
-            <motion.div
-                className="mt-2 alert" 
-                id="SentAlert"
-                role="alert"
-                animate={sentisOpen ? "open" : "closed"}
-                variants={variants}
-            >
-            </motion.div>
+
+            <AlertDanger 
+                text={dangerText} 
+                see={dangerAlert}
+                animate={{ opacity: 1, x: 0 }}
+                change={{ opacity: 0, x: -50 }}
+            />
         </div>
     );
 };
