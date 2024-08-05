d28c9fc67d904d2462658c0c0ab15fbc1e99defc
Kiril B <kirbou06012@gmail.com>
2024-08-05 14:22:50 -0300

    Updated fileNotAccessible

--- frontend/src/components/fileNotAccessible.jsx
+++ frontend/src/components/fileNotAccessible.jsx
@@ -1,9 +1,24 @@
-export default function FileNotAccessible(){
-    return(
-        <div className="w-screen">
-            <div className="mx-auto">
-                <p>The file you tried to acess is not acessible. Please make sure you are logged in, have permission to view the file, and try again.</p>
-            </div>
-        </div>  
-    )
+import { TbFaceIdError } from "react-icons/tb";
+
+export default function FileNotAccessible({ error=1 }){
+    if (error === 1){
+        return(
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                        
+                        </div>
+                        <div class="col">
+                            <div className="text-center">
+                                <TbFaceIdError size={150}/>
+                            </div>
+                            <p className="text-center">We're very sorry. Something went wrong. Please make sure the file exists, and you have permision to acess it, then try again.</p>
+                        </div>
+                        <div class="col">
+                        
+                        </div>
+                    </div>
+                </div>
+        )
+    }
 }
